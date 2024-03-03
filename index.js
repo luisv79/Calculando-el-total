ed65138cf8c4e0efb7eb0a18ef9d08e6f5d226fa
Luis Valladares <luisvalladares79@gmail.com>
2024-03-03 11:21:59 -0300

    subiendo el repositorio final

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-precio = 400000
+let precio = 400000
 
-precioSpan = document.querySelector(".precio-inicial");
+precioSpan = document.querySelector(".precio-inicial")
 precioSpan.innerHTML = precio
+
+let cantidadSpan = document.querySelector(".cantidad");
+let totalSpan = document.querySelector(".valor-total");
+
+
+function cantidadMas() {
+    span = document.querySelector('.cantidad')
+    cuentaMas = Number(span.innerHTML)
+    cuentaMas = cuentaMas + 1
+    span.innerHTML = cuentaMas;
+    cantidadTotal();
+  }
+
+  function cantidadMenos(){
+  span = document.querySelector('.cantidad')
+  cuentaMenos = Number(span.innerHTML)
+  cuentaMenos = cuentaMenos - 1
+  span.innerHTML = cuentaMenos
+  cantidadTotal();
+}
+
+function cantidadTotal() {
+    let cantidad = Number(cantidadSpan.innerHTML);
+    let total = precio * cantidad;
+    totalSpan.innerHTML = total;
+}
